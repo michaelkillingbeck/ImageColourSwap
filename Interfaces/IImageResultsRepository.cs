b5816f23da30fe12aec14581ae294f114f38176f
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2022-11-01 21:13:36 +0000

    Added PageLoad to Repository class.

--- Interfaces/IImageResultsRepository.cs
+++ Interfaces/IImageResultsRepository.cs
@@ -2,6 +2,7 @@ namespace Image_Colour_Swap.Interfaces;
 
 public interface IImageResultsRepository<T>
 {
+    Task<T> LoadPagedResults(string nextPage);
     Task<T> LoadResults(string id);
     Task<bool> SaveResults(T results);
 }
