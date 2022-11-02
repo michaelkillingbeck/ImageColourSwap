51f9cfdc05cdf10f437b8a7e9454fb83434a95bd
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2022-11-02 19:07:31 +0000

    Removed paged results method

--- Interfaces/IImageResultsRepository.cs
+++ Interfaces/IImageResultsRepository.cs
@@ -2,7 +2,6 @@ namespace Image_Colour_Swap.Interfaces;
 
 public interface IImageResultsRepository<T>
 {
-    Task<T> LoadPagedResults(string nextPage);
     Task<T> LoadResults(string id);
     Task<bool> SaveResults(T results);
 }
